76f3b745652affb845790afeeefbf51b0859e070
andrem <andreoandre@gmail.com>
2013-05-15 17:07:20 -0300

    dados coletados

--- system_collector.pl
+++ system_collector.pl
@@ -8,75 +8,82 @@ use warnings;
 use VMware::VIRuntime;
 use VMware::VILib;
 use Data::Dumper;
-use Time::HiRes qw(usleep nanosleep);
 
 $SIG{__DIE__} = sub{Util::disconnect() };
 
-my $PREFIX = "virtualMachine";
-              
-my %physical_host = ( 
+my $allCounters;
+my $type = 'sys';
+my $maxSample = 1;
+          
+my %physicalHost = ( 
     "server1.com" => [ "root", "X" ] , 
 );
 
-foreach my $host (keys %physical_host ) {
+foreach my $host (keys %physicalHost ) {
     Util::connect(
         "https://" . $host . "/sdk/webService", 
-        $physical_host{$host}->[0], $physical_host{$host}->[1] );
+        $physicalHost{$host}->[0], $physicalHost{$host}->[1] );
 
-    my @msgs = DiskPerfCollector($host);
+    my @msgs = GetPerformance($host);
     
-    foreach my $m (@msgs) {
-         print "$m";
-         #
-         # and there is time to kill today ....
-         #
-         usleep(2000);
-    }
-
     Util::disconnect();
 }
 
-sub DiskPerfCollector {
+sub GetPerformance {
     my ($ESXiServer) = @_;
-    my $type = 'sys';
     my $counters;
-    my @perf_metric_ids; 
-    my $all_counters;
-    my $perf_query_spec;
-    my $perf_data;
- 
-    my $host = Vim::find_entity_views(view_type => 'HostSystem', filter => {'name' => $ESXiServer});
-    my $perfmgr_view = Vim::get_view(mo_ref => Vim::get_service_content()->perfManager);
-    my $perfCounterInfo = $perfmgr_view->perfCounter;
-    my $availmetricid = $perfmgr_view->QueryAvailablePerfMetric(entity => $host);
+    my @perfMetricIds;
+    my $host = Vim::find_entity_view(view_type => "HostSystem", filter => {'name' => $ESXiServer});
+    my $perfmgrView = Vim::get_view(mo_ref => Vim::get_service_content()->perfManager);
+    my $providerSummary = $perfmgrView->QueryPerfProviderSummary(entity => $host);
+    my $perfCounterInfo = $perfmgrView->perfCounter;
+    my $availmetricid = $perfmgrView->QueryAvailablePerfMetric(entity => $host);
+    
     foreach (@$perfCounterInfo) {
         my $key = $_->key;
-        $all_counters->{ $key } = $_;
-        my $group_info = $_->groupInfo;
-        if ($group_info->key eq $type) {
+        $allCounters->{ $key } = $_;
+        my $groupInfo = $_->groupInfo;
+        if ($groupInfo->key eq $type) {
             $counters->{ $key } = $_;
         }
     }
     foreach (@$availmetricid) {
         if (exists $counters->{$_->counterId}) {
-             my $metric = PerfMetricId->new (counterId => $_->counterId,instance => (Opts::get_option('instance') || ''));
-             push @perf_metric_ids, $metric;
+             my $metric = PerfMetricId->new (counterId => $_->counterId,instance => '*');
+             push @perfMetricIds, $metric;
         }
-    }
-
-    my $historical_intervals = $perfmgr_view->historicalInterval;
-    my $provider_summary = $perfmgr_view->QueryPerfProviderSummary(entity => $host);
-    my @intervals;
+    } 
     
-    if ($provider_summary->refreshRate) {
-        push @intervals, $provider_summary->refreshRate;
-    }
+    my $perfQuerySpec;
+    $perfQuerySpec = PerfQuerySpec->new(entity => $host,
+                                        metricId => \@perfMetricIds,
+                                        'format' => 'csv',
+                                        intervalId => $providerSummary->refreshRate, 
+                                        maxSample => $maxSample );
+   
+    my $perfData;
+    eval {
+        $perfData = $perfmgrView->QueryPerf( querySpec => $perfQuerySpec);
+    };
 
-    foreach (@$historical_intervals) {
-        push @intervals, $_->samplingPeriod;
-    }
-    
-    $perf_query_spec = PerfQuerySpec->new(entity => $host, metricId => \@perf_metric_ids, 'format' => 'csv', intervalId => shift @intervals, maxSample => '');
+    PrintValues($perfData, $host);   
+}
 
-    print Dumper($perf_query_spec);
+sub PrintValues {
+    my ($data,$host) = @_;
+
+    foreach (@$data) {
+        my $hostname  = $host->name;
+        my $timestamp = $_->sampleInfoCSV; 
+        my $values    = $_->value;
+        foreach (@$values) {
+            my $counter = $allCounters->{$_->id->counterId};
+            print "Counter: " . $counter->nameInfo->label . "\n";
+            print "Description: " . $counter->nameInfo->summary . "\n";
+            print "Instance: " . $_->id->instance . "\n";
+            print "Units: " . $counter->unitInfo->label . "\n";
+            print "Value: " . $_->value . "\n";
+            print "-----------------------------------------------------\n";
+        }
+    }   
 }
